f7c91f3e9851acc4775b14f595943c285e43df96
massu-159 <magjtwjp@gmail.com>
2022-09-01 15:59:59 +0900

    create postData

---
+++ db/Post.ts
@@ -0,0 +1,21 @@
+export const publicPost = [
+  {
+    title: "Open article",
+    body: "This is the open article",
+  },
+  {
+    title: "Open article2",
+    body: "This is the open article2",
+  },
+];
+
+export const privatePost = [
+  {
+    title: "close article(JWT)",
+    body: "This is the close article",
+  },
+  {
+    title: "close article(JWT)2",
+    body: "This is the close article2",
+  },
+];
